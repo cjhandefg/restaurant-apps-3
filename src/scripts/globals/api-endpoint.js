7a14e54aa1339e8c0ce3442bc252a7caee2d4ee4
cjhandefg <89524146+cjhandefg@users.noreply.github.com>
2024-06-21 14:59:25 +0800

    Delete src directory

--- src/scripts/globals/api-endpoint.js
+++
@@ -1,7 +0,0 @@
-const API_ENDPOINT = {
-  LIST: "https://restaurant-api.dicoding.dev/list",
-  DETAIL: (id) => `https://restaurant-api.dicoding.dev/detail/${id}`,
-  REVIEW: "https://restaurant-api.dicoding.dev/review",
-};
-
-export default API_ENDPOINT;
--- src/scripts/globals/config.js
+++
@@ -1,7 +0,0 @@
-const CONFIG = {
-  BASE_IMAGE_URL: "https://restaurant-api.dicoding.dev/images/small/",
-  DATABASE_NAME: "resto-app-database",
-  DATABASE_VERSION: 1,
-  OBJECT_STORE_NAME: "restaurant",
-};
-export default CONFIG;
